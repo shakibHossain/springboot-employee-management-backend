7ebbe055ca8cf8ad5675ff6474177d1def63f75d
shakibHossain <shakib.hossain.sh@gmail.com>
2020-10-05 23:07:46 -0600

    add employee rest api added

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
